b90bfdc45f2fc22605b70f95f51101ea1b8d69de
Easy Learn Java <easylearnjava4u@gmail.com>
2024-06-23 01:15:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 
+document.querySelector('form').addEventListener('submit', function(event) {
+    var searchInput = document.querySelector('.search-bar input');
+    if (searchInput.value.trim() === '') {
+        event.preventDefault();
+        alert('Please enter a search term.');
+    }
+});
