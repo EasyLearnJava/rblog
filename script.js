0d6262109820c0fe57deb60339fe5d86bf707858
Easy Learn Java <easylearnjava4u@gmail.com>
2024-06-23 08:46:24 -0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-
-document.querySelector('form').addEventListener('submit', function(event) {
-    var searchInput = document.querySelector('.search-bar input');
-    if (searchInput.value.trim() === '') {
-        event.preventDefault();
-        alert('Please enter a search term.');
-    }
-});
